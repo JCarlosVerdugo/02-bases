8b2375d22d76accf59a4e7ac04e24349f7df93f8
Carlos Verdugo <carlosverdugo@proton.me>
2024-04-21 19:43:06 -0700

    ngif

--- src/app/heroes/heroe/heroe.component.ts
+++ src/app/heroes/heroe/heroe.component.ts
@@ -6,5 +6,35 @@ import { Component } from '@angular/core';
   styleUrl: './heroe.component.css'
 })
 export class HeroeComponent {
+  public name: string = 'ironman';
+  public age:  number = 45;
+
+
+  get capitalizedName(): string {
+    return 'Hola Mundo'
+  }
+
+
+  getHeroeDescription(): string {
+    return `${ this.name } - ${this.age}`;
+  }
+
+
+  changeHero(): void {
+    this.name = 'Spiderman'
+  }
+
+
+  changeAge(): void {
+    this.age = 25;
+  }
+
+
+  resetForm(): void {
+      this.name = 'ironman';
+      this.age = 45;
+
+      // document.querySelector('h1')!.innerHTML = '<h1>Desde angular</h1>'
+  }
 
 }
